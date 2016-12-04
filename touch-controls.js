03e36bcb46a5b81b6796feb8953058e4ec788b47
Thomas Balouet <Thomas Balouet>
2016-12-04 16:07:10 +0100

    added real touch models better handling of different buttons accessible

--- touch-controls.js
+++ touch-controls.js
@@ -1,5 +1,7 @@
-var LEFT_HAND_MODEL_URL = 'https://cdn.aframe.io/controllers/hands/leftHand.json';
-var RIGHT_HAND_MODEL_URL = 'https://cdn.aframe.io/controllers/hands/rightHand.json';
+var LEFT_TOUCH_CONTROLLER_MODEL_OBJ_URL = './models/touch_left.obj';
+var LEFT_TOUCH_CONTROLLER_MODEL_MTL_URL = './models/touch_left.mtl';
+var RIGHT_TOUCH_CONTROLLER_MODEL_OBJ_URL = './models/touch_right.obj';
+var RIGHT_TOUCH_CONTROLLER_MODEL_MTL_URL = './models/touch_right.mtl';
 
 /**
  * Oculus Touch Controls Component
@@ -19,119 +21,94 @@ AFRAME.registerComponent('touch-controls', {
 
   // buttonId
   // 0 - trackpad
-  // 1 - trigger ( intensity value from 0.5 to 1 )
+  // 1 - trigger
   // 2 - grip
-  // 3 - menu ( dispatch but better for menu options )
-  // 4 - system ( never dispatched on this layer )
+  // 3 - menu1 ( either A on right controler or X on the left one )
+  // 4 - menu2 ( either B on right controler or Y on the left one )
+  // 5 - surface ( the flat touch surface next to the buttons )
   mapping: {
-    axis0: 'trackpad',
-    axis1: 'trackpad',
-    button0: 'trackpad',
-    button1: 'trigger',
-    button2: 'grip',
-    button3: 'menu',
-    button4: 'system'
+    axis0   : 'trackpad',
+    axis1   : 'trackpad',
+    button0 : 'trackpad',
+    button1 : 'trigger',
+    button2 : 'grip',
+    button3 : 'menu1',
+    button4 : 'menu2',
+    button5 : 'surface'
   },
 
   init: function () {
     var self = this;
-    this.animationActive = 'pointing';
-    this.onButtonDown    = function (evt) { self.onButtonEvent(evt.detail.id, 'down'); };
-    this.onButtonUp      = function (evt) { self.onButtonEvent(evt.detail.id, 'up'); };  },
+    this.animationActive    = 'pointing';
+    this.onButtonDown       = function (evt) { self.onButtonEvent(evt.detail.id, 'down'); };
+    this.onButtonUp         = function (evt) { self.onButtonEvent(evt.detail.id, 'up'); };
+    this.onButtonTouchStart = function (evt) { self.onButtonEvent(evt.detail.id, 'touchstart'); };
+    this.onButtonTouchEnd   = function (evt) { self.onButtonEvent(evt.detail.id, 'touchend'); };
+    this.onModelLoaded = this.onModelLoaded.bind(this);
+  },
 
   play: function () {
     var el = this.el;
     el.addEventListener('buttondown', this.onButtonDown);
     el.addEventListener('buttonup', this.onButtonUp);
+    el.addEventListener('touchstart', this.onButtonTouchStart);
+    el.addEventListener('touchend', this.onButtonTouchEnd);
+    el.addEventListener('model-loaded', this.onModelLoaded);
   },
 
   pause: function () {
     var el = this.el;
     el.removeEventListener('buttondown', this.onButtonDown);
     el.removeEventListener('buttonup', this.onButtonUp);
+    el.removeEventListener('touchstart', this.onButtonTouchStart);
+    el.removeEventListener('touchend', this.onButtonTouchEnd);
+    el.removeEventListener('model-loaded', this.onModelLoaded);
   },
 
   update: function () {
     var el = this.el;
     var data = this.data;
 
-    var modelUrl;
+    var objUrl, mtlUrl;
     if (data.hand === 'left') {
-      modelUrl = 'url(' + LEFT_HAND_MODEL_URL + ')';
+      objUrl = 'url(' + LEFT_TOUCH_CONTROLLER_MODEL_OBJ_URL + ')';
+      mtlUrl = 'url(' + LEFT_TOUCH_CONTROLLER_MODEL_MTL_URL + ')';
     } else {
-      modelUrl = 'url(' + RIGHT_HAND_MODEL_URL + ')';
+      objUrl = 'url(' + RIGHT_TOUCH_CONTROLLER_MODEL_OBJ_URL + ')';
+      mtlUrl = 'url(' + RIGHT_TOUCH_CONTROLLER_MODEL_MTL_URL + ')';
     }
     // handId: 0 - right, 1 - left
     var id = "Oculus Touch " + (data.hand === 'left' ? "(Left)" : "(Right)");
     el.setAttribute('tracked-controls', 'id', id);
-    el.setAttribute('blend-character-model', modelUrl);
+    el.setAttribute('obj-model', {obj: objUrl, mtl: mtlUrl});
+  },
+
+  onModelLoaded: function (evt) {
+    var controllerObject3D = evt.detail.model;
+    var buttonMeshes;
+    if (!this.data.model) { return; }
+
+    var leftHand = this.data.hand === "left";
+    buttonMeshes          = this.buttonMeshes = {};
+    
+    buttonMeshes.grip     = controllerObject3D.getObjectByName(leftHand ? 'grip tooche1 group3' : "grip tooche group4");
+    buttonMeshes.trackpad = controllerObject3D.getObjectByName(leftHand ? 'tooche1 group3 control_surface group2 thumb_stick' : "tooche group4 control_surface group2 thumb_stick");
+    buttonMeshes.trigger  = controllerObject3D.getObjectByName(leftHand ? 'tooche1 group3 trigger' : "tooche group4 trigger");
+    buttonMeshes.menu1    = controllerObject3D.getObjectByName(leftHand ? 'tooche1 group3 control_surface group2 button2' : 'tooche group4 control_surface group2 button2');
+    buttonMeshes.menu2    = controllerObject3D.getObjectByName(leftHand ? 'tooche1 group3 control_surface group2 button3' : 'tooche group4 control_surface group2 button3');
+    buttonMeshes.surface  = controllerObject3D.getObjectByName(leftHand ? 'tooche1 group3 face control_surface group2' : 'tooche group4 face control_surface group2');
+
+    // Offset pivot point
+    controllerObject3D.position.set(0, -0.015, 0.04);
   },
 
   onButtonEvent: function (id, evtName) {
+    var trackedControler = this.el.components["tracked-controls"];
+    if(trackedControler.controller){
+      console.log(trackedControler.controller.id, trackedControler.buttonStates);
+    }
     var buttonName = this.mapping['button' + id];
     this.el.emit(buttonName + evtName);
     console.log("button event", buttonName, evtName);
-    this.handleButton(buttonName, evtName);
-  },
-
- /** Play the model animations based on the pressed button and kind of event.
-   *
-   * @param {string} button the name of the button
-   * @param {string} evt the event associated to the button
-   */
-  handleButton: function (button, evt) {
-    var el = this.el;
-    var isPressed = evt === 'down';
-    switch (button) {
-      case 'trackpad':
-        if (isPressed === this.trackpadPressed) { return; }
-        this.trackpadPressed = isPressed;
-        this.playAnimation('thumb', !isPressed);
-        evt = isPressed ? 'thumbup' : 'thumbdown';
-        el.emit(evt);
-        break;
-      case 'trigger':
-        if (isPressed === this.triggerPressed) { return; }
-        this.triggerPressed = isPressed;
-        var animation = (this.gripPressed ? "close" : "pointing");
-        this.playAnimation(animation, !isPressed);
-        evt = isPressed ? 'pointup' : 'pointdown';
-        el.emit(evt);
-        break;
-      case 'grip':
-        if (isPressed === this.gripPressed) { return; }
-        this.gripPressed = isPressed;
-        var animation = (this.triggerPressed ? "close" : "pointing");
-        this.playAnimation(animation, !isPressed);
-        evt = isPressed ? 'gripclose' : 'gripopen';
-        el.emit(evt);
-        break;
-    }
-  },
-
-  /**
-  * Play the hand animations based on button state.
-  *
-  * @param {string} animation - the name of the animation.
-  * @param {string} reverse - It the animation has to play in reverse.
-  */
-  playAnimation: function (animation, reverse) {
-    var animationActive = this.animationActive;
-    var timeScale = 1;
-    var mesh = this.el.getObject3D('mesh');
-    if (!mesh) { return; }
-
-    // determine direction of the animation.
-    if (reverse) { timeScale = -1; }
-
-    // stop current animation.
-    if (animationActive) { mesh.play(animationActive, 0); }
-
-    // play new animation.
-    mesh.mixer.clipAction(animation).loop = 2200;
-    mesh.mixer.clipAction(animation).clampWhenFinished = true;
-    mesh.mixer.clipAction(animation).timeScale = timeScale;
-    mesh.play(animation, 1);
-    this.animationActive = animation;
   }
 });
